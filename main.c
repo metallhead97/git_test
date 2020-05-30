7ea285875d8860f9f67acf825d8c9d33eacfde7f
Timophey <metallhead97@bk.ru>
2020-05-30 15:29:51 +0300

    my first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+    char arr[] = {'1', '2'};
+    int value = 0;
+
+    for(size_t i = 0; i < sizeof(arr) / sizeof(*arr); ++i){
+
+        value *= 10;
+        value += arr[i] - '0';
+		
+    }
+    printf("%d", value);
+    return 0;
+}
